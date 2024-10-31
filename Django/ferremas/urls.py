bb7f5f76dadb0bd9752ecb0d38f142cb168164a2
alovidal <alo.vidal@duocuc.cl>
2024-10-31 10:05:33 -0300

    Base estructura

--- Django/ferremas/urls.py
+++ Django/ferremas/urls.py
@@ -3,4 +3,8 @@ from ferremas import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("tienda", views.tienda, name="tienda"),
+    path("login", views.login, name="login"),
+    path("carro", views.carro, name="carro"),
+    path("seguimiento", views.seguimiento, name="seguimiento"),
 ]
