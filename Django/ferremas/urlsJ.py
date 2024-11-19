d49cbbe29affdb027a85ed6bec463ee8963b7b3a
relaxcode425 <jamesvergaracoronado@gmail.com>
2024-11-19 17:17:57 -0300

    aaaaa

--- Django/ferremas/urlsJ.py
+++ Django/ferremas/urlsJ.py
@@ -2,5 +2,5 @@ from django.urls import path
 from ferremas import viewsJ
 
 urlpatterns = [
-    
+    path('ver_ordenes/', viewsJ.verordenes, name='ver_ordenes'),
 ]
--- Django/ferremas/viewsJ.py
+++ Django/ferremas/viewsJ.py
@@ -2,3 +2,15 @@ from django.shortcuts import render
 from .models import *
 
 # Create your views here.
+
+
+def verordenes(request):
+    pedidos = Pedido.objects.all()
+    detalles = DetallePedido.objects.all()
+    ordenes = Ordenes.objects.all()
+    context = {
+        "pedidos":pedidos,
+        "detalles":detalles,
+        "ordenes":ordenes
+    }
+    return render(request, "pages/verordenes.html", context)
